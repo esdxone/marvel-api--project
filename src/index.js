606d295faace7a933da280391655e70a0f5fcd08
esdxone <roaringgate@gmail.com>
2022-08-16 20:40:33 +0300

    Rewrite project on redux

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import {createRoot} from 'react-dom/client';
 import App from './components/app/app';
+import { Provider } from 'react-redux';
+import store from './store';
 import './styles/style.scss';
 
 const root = createRoot(document.getElementById('root'));
 root.render(
+    <Provider store={store}>
             <App/>
+            </Provider>
 
 );
